20b3e538d48ac4781f7821d03de1d5452fc2b935
Royston1999 <56835953+Royston1999@users.noreply.github.com>
2022-01-17 12:44:57 +0000

    replace increments with sliders

--- src/SaberTailorLeftHand.cpp
+++ src/SaberTailorLeftHand.cpp
@@ -3,16 +3,20 @@
 #include "SaberTailorConfig.hpp"
 #include "UnityEngine/RectOffset.hpp"
 #include "SaberTailorHelper.hpp"
+#include "IncrementSlider.hpp"
+#include "GlobalNamespace/SharedCoroutineStarter.hpp"
+#include <cmath>
+#include "UnityEngine/WaitForFixedUpdate.hpp"
 
 DEFINE_TYPE(SaberTailor::Views, SaberTailorLeftHand);
 
 UnityEngine::UI::VerticalLayoutGroup* leftsabercontainer;
-QuestUI::IncrementSetting* posX;
-QuestUI::IncrementSetting* posY;
-QuestUI::IncrementSetting* posZ;
-QuestUI::IncrementSetting* rotX;
-QuestUI::IncrementSetting* rotY;
-QuestUI::IncrementSetting* rotZ;
+SaberTailor::IncrementSlider* posX;
+SaberTailor::IncrementSlider* posY;
+SaberTailor::IncrementSlider* posZ;
+SaberTailor::IncrementSlider* rotX;
+SaberTailor::IncrementSlider* rotY;
+SaberTailor::IncrementSlider* rotZ;
 Array<TMPro::TextMeshProUGUI*>* incText;
 
 void SaberTailor::Views::SaberTailorLeftHand::DidActivate(
@@ -34,38 +38,39 @@ void SaberTailor::Views::SaberTailorLeftHand::DidActivate(
         CreateUIButton(somebuttons->get_transform(), "Export to Base Game", [](){
             TransferHelper::exportToBaseGame(0);
         });
-        posX = CreateIncrementSetting(leftsabercontainer->get_transform(), "Position X", 1, 0.1f, SaberTailorMain::config.leftPosX, -10.0f, 10.0f, [](float value){
-            IncrementHelper::setIncrementText(posX, value);
-            float num = IncrementHelper::fixDumbNumberThing(value);
-            setFloat(getConfig().config, "leftPosX", num); getConfig().Write();
+        posX = SaberTailor::IncrementSlider::CreateIncrementSlider(leftsabercontainer->get_transform(), "Position X", 0.1f, SaberTailorMain::config.leftPosX, -10, 10, [](float value){
+            setFloat(getConfig().config, "leftPosX", value); getConfig().Write();
             ConfigHelper::LoadConfig(SaberTailorMain::config, getConfig().config);
+            posX->sliderComponent->slider->GetComponentInChildren<TMPro::TextMeshProUGUI*>()->set_text(il2cpp_utils::newcsstr(IncrementHelper::Round(SaberTailorMain::config.leftPosX, 1) + " cm"));
         });
-        posY = CreateIncrementSetting(leftsabercontainer->get_transform(), "Position Y", 1, 0.1f, SaberTailorMain::config.leftPosY, -10.0f, 10.0f, [](float value){
-            IncrementHelper::setIncrementText(posY, value);
-            float num = IncrementHelper::fixDumbNumberThing(value);
-            setFloat(getConfig().config, "leftPosY", num); getConfig().Write();
+        posY = SaberTailor::IncrementSlider::CreateIncrementSlider(leftsabercontainer->get_transform(), "Position Y", 0.1f, SaberTailorMain::config.leftPosY, -10, 10, [](float value){
+            setFloat(getConfig().config, "leftPosY", value); getConfig().Write();
             ConfigHelper::LoadConfig(SaberTailorMain::config, getConfig().config);
+            posY->sliderComponent->slider->GetComponentInChildren<TMPro::TextMeshProUGUI*>()->set_text(il2cpp_utils::newcsstr(IncrementHelper::Round(SaberTailorMain::config.leftPosY, 1) + " cm"));
         });
-        posZ = CreateIncrementSetting(leftsabercontainer->get_transform(), "Position Z", 1, 0.1f, SaberTailorMain::config.leftPosZ, -10.0f, 10.0f, [](float value){
-            IncrementHelper::setIncrementText(posZ, value);
-            float num = IncrementHelper::fixDumbNumberThing(value);
-            setFloat(getConfig().config, "leftPosZ", num); getConfig().Write();
+        posZ = SaberTailor::IncrementSlider::CreateIncrementSlider(leftsabercontainer->get_transform(), "Position Z", 0.1f, SaberTailorMain::config.leftPosZ, -10, 10, [](float value){
+            setFloat(getConfig().config, "leftPosZ", value); getConfig().Write();
             ConfigHelper::LoadConfig(SaberTailorMain::config, getConfig().config);
+            posZ->sliderComponent->slider->GetComponentInChildren<TMPro::TextMeshProUGUI*>()->set_text(il2cpp_utils::newcsstr(IncrementHelper::Round(SaberTailorMain::config.leftPosZ, 1) + " cm"));
         });
-        rotX = CreateIncrementSetting(leftsabercontainer->get_transform(), "Rotation X", 0, 1.0f, SaberTailorMain::config.leftRotX, -180.0f, 180.0f, [](float value){
-            IncrementHelper::setRotIncrementText(rotX, value);
-            setInt(getConfig().config, "leftRotX", value); getConfig().Write();
+        rotX = SaberTailor::IncrementSlider::CreateIncrementSlider(leftsabercontainer->get_transform(), "Rotation X", 1, SaberTailorMain::config.leftRotX, -180, 180, [](float value){
+            int x = std::round(value);
+            setInt(getConfig().config, "leftRotX", x); getConfig().Write();
             ConfigHelper::LoadConfig(SaberTailorMain::config, getConfig().config);
+            rotX->sliderComponent->slider->GetComponentInChildren<TMPro::TextMeshProUGUI*>()->set_text(il2cpp_utils::newcsstr(std::to_string(SaberTailorMain::config.leftRotX) + " deg"));
         });
-        rotY = CreateIncrementSetting(leftsabercontainer->get_transform(), "Rotation Y", 0, 1.0f, SaberTailorMain::config.leftRotY, -180.0f, 180.0f, [](float value){
-            IncrementHelper::setRotIncrementText(rotY, value);
-            setInt(getConfig().config, "leftRotY", value); getConfig().Write();
+        rotY = SaberTailor::IncrementSlider::CreateIncrementSlider(leftsabercontainer->get_transform(), "Rotation Y", 1, SaberTailorMain::config.leftRotY, -180, 180, [](float value){
+            int x = std::round(value);
+            setInt(getConfig().config, "leftRotY", x); getConfig().Write();
             ConfigHelper::LoadConfig(SaberTailorMain::config, getConfig().config);
+            rotY->sliderComponent->slider->GetComponentInChildren<TMPro::TextMeshProUGUI*>()->set_text(il2cpp_utils::newcsstr(std::to_string(SaberTailorMain::config.leftRotY) + " deg"));
         });
-        rotZ = CreateIncrementSetting(leftsabercontainer->get_transform(), "Rotation Z", 0, 1.0f, SaberTailorMain::config.leftRotZ, -180.0f, 180.0f, [](float value){
-            IncrementHelper::setRotIncrementText(rotZ, value);
-            setInt(getConfig().config, "leftRotZ", value); getConfig().Write();
+
+        rotZ = SaberTailor::IncrementSlider::CreateIncrementSlider(leftsabercontainer->get_transform(), "Rotation Z", 1, SaberTailorMain::config.leftRotZ, -180, 180, [](float value){
+            int x = std::round(value);
+            setInt(getConfig().config, "leftRotZ", x); getConfig().Write();
             ConfigHelper::LoadConfig(SaberTailorMain::config, getConfig().config);
+            rotZ->sliderComponent->slider->GetComponentInChildren<TMPro::TextMeshProUGUI*>()->set_text(il2cpp_utils::newcsstr(std::to_string(SaberTailorMain::config.leftRotZ) + " deg"));
         });
 
         HorizontalLayoutGroup* somemorebuttons = QuestUI::BeatSaberUI::CreateHorizontalLayoutGroup(leftsabercontainer->get_transform());
@@ -80,19 +85,19 @@ void SaberTailor::Views::SaberTailorLeftHand::DidActivate(
         });
     }
         QuestUI::ArrayUtil::First(UnityEngine::Resources::FindObjectsOfTypeAll<UnityEngine::UI::Button*>(), [] (UnityEngine::UI::Button* x) { return to_utf8(csstrtostr(x->get_name())) == "BackButton"; })->get_gameObject()->SetActive(false);
-        posX->CurrentValue = SaberTailorMain::config.leftPosX;
-        posY->CurrentValue = SaberTailorMain::config.leftPosY;
-        posZ->CurrentValue = SaberTailorMain::config.leftPosZ;
-        rotX->CurrentValue = SaberTailorMain::config.leftRotX;
-        rotY->CurrentValue = SaberTailorMain::config.leftRotY;
-        rotZ->CurrentValue = SaberTailorMain::config.leftRotZ;
+        posX->sliderComponent->set_value(SaberTailorMain::config.leftPosX);
+        posY->sliderComponent->set_value(SaberTailorMain::config.leftPosY);
+        posZ->sliderComponent->set_value(SaberTailorMain::config.leftPosZ);
+        rotX->sliderComponent->set_value(SaberTailorMain::config.leftRotX);
+        rotY->sliderComponent->set_value(SaberTailorMain::config.leftRotY);
+        rotZ->sliderComponent->set_value(SaberTailorMain::config.leftRotZ);
 
-        IncrementHelper::setIncrementText(posX, SaberTailorMain::config.leftPosX);
-        IncrementHelper::setIncrementText(posY, SaberTailorMain::config.leftPosY);
-        IncrementHelper::setIncrementText(posZ, SaberTailorMain::config.leftPosZ);
-        IncrementHelper::setRotIncrementText(rotX, SaberTailorMain::config.leftRotX);
-        IncrementHelper::setRotIncrementText(rotY, SaberTailorMain::config.leftRotY);
-        IncrementHelper::setRotIncrementText(rotZ, SaberTailorMain::config.leftRotZ);
+        GlobalNamespace::SharedCoroutineStarter::get_instance()->StartCoroutine(reinterpret_cast<custom_types::Helpers::enumeratorT*>(custom_types::Helpers::CoroutineHelper::New(IncrementHelper::forceUpdateSliderText(posX, IncrementHelper::Round(SaberTailorMain::config.leftPosX, 1) + " cm"))));
+        GlobalNamespace::SharedCoroutineStarter::get_instance()->StartCoroutine(reinterpret_cast<custom_types::Helpers::enumeratorT*>(custom_types::Helpers::CoroutineHelper::New(IncrementHelper::forceUpdateSliderText(posY, IncrementHelper::Round(SaberTailorMain::config.leftPosY, 1) + " cm"))));
+        GlobalNamespace::SharedCoroutineStarter::get_instance()->StartCoroutine(reinterpret_cast<custom_types::Helpers::enumeratorT*>(custom_types::Helpers::CoroutineHelper::New(IncrementHelper::forceUpdateSliderText(posZ, IncrementHelper::Round(SaberTailorMain::config.leftPosZ, 1) + " cm"))));
+        GlobalNamespace::SharedCoroutineStarter::get_instance()->StartCoroutine(reinterpret_cast<custom_types::Helpers::enumeratorT*>(custom_types::Helpers::CoroutineHelper::New(IncrementHelper::forceUpdateSliderText(rotX, std::to_string(SaberTailorMain::config.leftRotX) + " deg"))));
+        GlobalNamespace::SharedCoroutineStarter::get_instance()->StartCoroutine(reinterpret_cast<custom_types::Helpers::enumeratorT*>(custom_types::Helpers::CoroutineHelper::New(IncrementHelper::forceUpdateSliderText(rotY, std::to_string(SaberTailorMain::config.leftRotY) + " deg"))));
+        GlobalNamespace::SharedCoroutineStarter::get_instance()->StartCoroutine(reinterpret_cast<custom_types::Helpers::enumeratorT*>(custom_types::Helpers::CoroutineHelper::New(IncrementHelper::forceUpdateSliderText(rotZ, std::to_string(SaberTailorMain::config.leftRotZ) + " deg"))));
         
         SaberTailorMain::config.currentPosXLeft = SaberTailorMain::config.leftPosX;
         SaberTailorMain::config.currentPosYLeft = SaberTailorMain::config.leftPosY;
