277c175f73f9d5e2bfc890ca51afbcbf7349393b
royston thorndycraft <18059207@brookes.ac.uk>
2023-01-02 20:33:02 +0900

    reordered the settings loading so the page tabs are loaded in first

--- include/UI/SettingsViewController.hpp
+++ include/UI/SettingsViewController.hpp
@@ -11,4 +11,6 @@ DECLARE_CLASS_CODEGEN(SaberTailor::Views, SettingsViewController, HMUI::ViewCont
     DECLARE_OVERRIDE_METHOD(void, DidActivate, il2cpp_utils::FindMethodUnsafe("HMUI", "ViewController", "DidActivate", 3), bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
     public:
         custom_types::Helpers::Coroutine CreateTabs();
+        std::vector<UnityEngine::GameObject*> tabs;
+        int selectedTab;
 )
--- src/UI/SettingsViewController.cpp
+++ src/UI/SettingsViewController.cpp
@@ -15,36 +15,30 @@ using namespace custom_types::Helpers;
 
 DEFINE_TYPE(SaberTailor::Views, SettingsViewController);
 
-int selectedTab;
-std::vector<UnityEngine::GameObject*> settingsTabs;
-
 void SaberTailor::Views::SettingsViewController::DidActivate(bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling) {
     if (firstActivation) {
-        selectedTab = 0;
-        settingsTabs.clear();
         GlobalNamespace::SharedCoroutineStarter::get_instance()->StartCoroutine(CoroutineHelper::New(CreateTabs()));
     }
 }
 
 Coroutine SaberTailor::Views::SettingsViewController::CreateTabs(){
-    auto tabs = &settingsTabs;
     ArrayW<StringW> tabNames(std::initializer_list<StringW>{"Main Settings", "Helper", "Scale", "Trail", "Profiles", "Experimental"});
-    QuestUI::BeatSaberUI::CreateTextSegmentedControl(get_transform(), {75, 7}, tabNames, [tabs](int index){
-        (*tabs)[selectedTab]->SetActive(false);
-        (*tabs)[index]->SetActive(true);
+    QuestUI::BeatSaberUI::CreateTextSegmentedControl(get_transform(), {75, 7}, tabNames, [&](int index){
+        tabs[selectedTab]->SetActive(false);
+        tabs[index]->SetActive(true);
         selectedTab = index;
     });
     co_yield nullptr;
-    tabs->push_back(MainSettings::CreateMainSettings(get_transform())); // tab 1
+    tabs.push_back(MainSettings::CreateMainSettings(get_transform())); // tab 1
     co_yield nullptr;
-    tabs->push_back(CSH::CreateCSHSettings(get_transform())); // tab 2
+    tabs.push_back(CSH::CreateCSHSettings(get_transform())); // tab 2
     co_yield nullptr;
-    tabs->push_back(Scale::CreateScaleSettings(get_transform())); // tab 3
+    tabs.push_back(Scale::CreateScaleSettings(get_transform())); // tab 3
     co_yield nullptr;
-    tabs->push_back(Trail::CreateTrailSettings(get_transform())); // tab 4
+    tabs.push_back(Trail::CreateTrailSettings(get_transform())); // tab 4
     co_yield nullptr;
-    tabs->push_back(Profile::CreateProfileSettings(get_transform())); // tab 5
+    tabs.push_back(Profile::CreateProfileSettings(get_transform())); // tab 5
     co_yield nullptr;
-    tabs->push_back(Experimental::CreateExperimentalSettings(get_transform())); // tab 6
+    tabs.push_back(Experimental::CreateExperimentalSettings(get_transform())); // tab 6
     co_return;
 }
