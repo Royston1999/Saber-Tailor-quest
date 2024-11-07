5667671af49ce030a92d33db850207c31e90a545
Royston1999 <royston.thorndycraft@gmail.com>
2024-11-07 07:25:13 +0000

    update release scripts

---
+++ draftrelease.ps1
@@ -0,0 +1,13 @@
+$mod = "./mod.json"
+
+qpm qmod manifest
+
+$modJson = Get-Content $mod -Raw | ConvertFrom-Json
+
+$modName = $modJson.name
+$modVersion = "v" $modJson.version
+$bsVersion = "v" $modJson.packageVersion.split("_")[0]
+ 
+Write-Host "Initiating workflow for $modName $modVersion for Beat Saber $bsVersion"
+
+gh workflow run build-ndk.yml -f version=$modVersion -f bs_version=$bsVersion
