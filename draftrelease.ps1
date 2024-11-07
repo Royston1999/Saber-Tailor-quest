3f986745f32191e533fc9ceaf49706851ea9db09
Royston1999 <royston.thorndycraft@gmail.com>
2024-11-07 07:26:31 +0000

    fix draft script

--- draftrelease.ps1
+++ draftrelease.ps1
@@ -5,8 +5,8 @@ qpm qmod manifest
 $modJson = Get-Content $mod -Raw | ConvertFrom-Json
 
 $modName = $modJson.name
-$modVersion = "v" $modJson.version
-$bsVersion = "v" $modJson.packageVersion.split("_")[0]
+$modVersion = "v" + $modJson.version
+$bsVersion = "v" + $modJson.packageVersion.split("_")[0]
  
 Write-Host "Initiating workflow for $modName $modVersion for Beat Saber $bsVersion"
 
